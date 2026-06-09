2d7d10e62bbf6e46d69b850552d828b24bfafc56
June-UwU <williamreji66@gmail.com>
2026-06-09 23:13:51 +0530

    [MEDUSA] timing support 	1. Added timed log support. 	2. Added example for documentation. 	3. minor rework for logging infra. 	4. minor bug fix. 	5. added color disable support.
    Change-Id: I19c4b3ea951104f0359e168cb7719ac0cad3935d
    Signed-off-by: June-UwU <williamreji66@gmail.com>

--- src/medusa.cxx
+++ src/medusa.cxx
@@ -6,8 +6,58 @@
 
 namespace medusa {
 
+#define write_to_console(format, ...)      \
+  if (level >= log_config.console_level) { \
+    printf(format, __VA_ARGS__);           \
+  }
+#define write_to_file(file, format, ...)                       \
+  if (nullptr != log_file && level >= log_config.file_level) { \
+    fprintf(file, format, __VA_ARGS__);                        \
+  };
+
 config log_config;
 FILE* log_file = nullptr;
+bool initialized = false;
+
+const char* get_color_code(log_level level) {
+  if (false == log_config.use_color) {
+    return "";
+  }
+
+  const char* color_code = nullptr;
+  switch (level) {
+    case log_level::log:
+      color_code = "\x1b[37m";
+      break;
+    case log_level::paranoid:
+      color_code = "\x1b[2;37m";
+      break;
+    case log_level::debug:
+      color_code = "\x1b[1;37m";
+      break;
+    case log_level::warning:
+      color_code = "\x1b[33m";
+      break;
+    case log_level::fatal:
+      color_code = "\x1b[1;31m";
+      break;
+    default:
+      color_code = "\x1b[0m";
+      break;
+  }
+  return color_code;
+}
+
+double get_elapsed_seconds() {
+  if (false == initialized) {
+    return 0.0;
+  }
+  const std::chrono::time_point<std::chrono::high_resolution_clock> time_now =
+      std::chrono::high_resolution_clock::now();
+  const std::chrono::duration<double> time_elapsed =
+      time_now - log_config.start_time;
+  return time_elapsed.count();
+}
 
 void write_banner() {
   write_log(log_level::log, "Medusa Version %d.%d.%d\n",
@@ -32,59 +82,42 @@ void initialize(const medusa::config& conf) {
   } else {
     write_log(log_level::log, "log file : %s\n", log_config.log_file.c_str());
   }
+  log_config.start_time = std::chrono::high_resolution_clock::now();
+  initialized = true;
 }
 
 void write_log(medusa::log_level level, const char* format, ...) {
-  char buffer[512];
-  uint64_t offset = 0;
+  static char buffer[512];
 
-  const char* color_code = nullptr;
-  switch (level) {
-    case log_level::log:
-      color_code = "\x1b[37m";
-      break;
-    case log_level::paranoid:
-      color_code = "\x1b[2;37m";
-      break;
-    case log_level::debug:
-      color_code = "\x1b[1;37m";
-      break;
-    case log_level::warning:
-      color_code = "\x1b[33m";
-      break;
-    case log_level::fatal:
-      color_code = "\x1b[1;31m";
-      break;
-    default:
-      color_code = "\x1b[0m";
-      break;
-  }
-
-  if (level < log_config.console_level) {
-    goto file_log;
-  }
   va_list args;
   va_start(args, format);
-  offset = vsnprintf(buffer, 512, format, args);
+  vsnprintf(buffer, 512, format, args);
   va_end(args);
 
-  printf("%s%s\x1b[0m", color_code, buffer);
+  const char* CLEAR_COLOR = log_config.use_color ? "\x1b[0m" : "";
+  write_to_console("%s%s%s", get_color_code(level), buffer, CLEAR_COLOR);
+  write_to_file(log_file, "%s", buffer);
+}
 
-file_log:
+void write_timed_log(medusa::log_level level, const char* format, ...) {
+  static char buffer[512];
 
-  if (nullptr == log_file || level < log_config.file_level) {
-    goto log_exit;
-  }
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buffer, 512, format, args);
+  va_end(args);
 
-  fprintf(log_file, "%s", buffer);
-log_exit:
-  return;
+  const char* TIME_COLOR = log_config.use_color ? "\033[1;32m" : "";
+  const char* CLEAR_COLOR = log_config.use_color ? "\x1b[0m" : "";
+  write_to_console("%s[%lf] %s%s%s", TIME_COLOR, get_elapsed_seconds(),
+                   get_color_code(level), buffer, CLEAR_COLOR);
+  write_to_file(log_file, "%s", buffer);
 }
 
 void deinitialize() {
   if (true == log_config.write_outro) {
-    write_log(log_level::log,
-              "/********************* END OF LOG *********************/\n");
+    double time = get_elapsed_seconds();
+    write_log(log_level::log, "took %lf seconds.\n", time);
   }
 
   if (nullptr != log_file) {
